7c3c85430d53ef820353172d0054252f6df5296d
P-Sale <paul.sale@tlgcohort.com>
2023-02-05 17:48:57 -0800

    initial files for website

---
+++ script.js
@@ -0,0 +1,105 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+var download = document.getElementById("download")
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("photo1");
+var modalImg = document.getElementById("img01");
+img.addEventListener("click", function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  download.href = this.src; 
+})
+
+var img = document.getElementById("photo2");
+var modalImg = document.getElementById("img01");
+img.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  })
+
+var img = document.getElementById("photo3");
+var modalImg = document.getElementById("img01");
+img.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  })
+
+var img = document.getElementById("photo4");
+var modalImg = document.getElementById("img01");
+img.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  })
+
+var img = document.getElementById("photo5");
+var modalImg = document.getElementById("img01");
+img.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  })
+
+var img = document.getElementById("photo6");
+var modalImg = document.getElementById("img01");
+img.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  })
+/*
+var img = document.getElementById("item7");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+var img = document.getElementById("item8");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+var img = document.getElementById("item9");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+var img = document.getElementById("item10");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+var img = document.getElementById("item11");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+var img = document.getElementById("item12");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+*/
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var spanDownload = document.getElementById("download");
+
+ //When the user clicks on <span> (x), close the modal
+ span.addEventListener("click", function() {
+     modal.style.display = "none";
+     download.href= "";
+ })
+
+modal.addEventListener("click", function(){
+    modal.style.display = "none";
+    download.href= "";
+})
